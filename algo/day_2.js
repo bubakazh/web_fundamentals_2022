c10c96aae76e91815eb491a224ea35044523156b
bubakazh <troymanansala@gmail.com>
2022-08-16 12:29:23 -0700

    wip_learnning

---
+++ algo/day_2.js
@@ -0,0 +1,50 @@
+// variables
+var full_name; //DECLARING A VARIABLE
+full_name = "Joseph Allen Troy de Guia Manansala" //INITIALIZING A VARIABLE
+var nick_name = "Troy Manansala" //DECLARING AND INITIALIZING A VARIABLE
+//
+//VARIABLE NAMES CAN BE WHATEVER WE WANT THEM TO BE
+var age = 33 //INTEGERS
+var awake = true; //BOOLEAN, T F
+var height = 5.9; //FLOATS
+
+//ADDITION
+age = age + 1
+age += 1
+//THESE TWO LINES OF CODE PERFORM THE EXACT SAME WAY. IT IS MERELY A DIFFERENCE IN SYNTAX.
+
+//SUBTRACTION
+age = age - 1
+age -= 1
+
+//MULTIPLICATION AND DIVISION
+age = age * 2
+age *= 2
+
+age = age / 3
+age /= 3
+
+console.log(age)
+
+//CONCATETNATION: ADDING STRINGS TOGETHER
+var first_name = "Troy"
+var last_name = "Manansala"
+console.log(first_name + last_name)
+
+//LOOPS
+
+//FOR LOOPS
+//DRY - DON'T REPEAT YOURSELF
+for(var index = 0; index < 100; index ++){
+    console.log(index)
+    console.log("BOTTOM OF LOOP")
+}
+
+//FOR LOOP PREDICTIONS
+//6
+//hello hello
+
+//0 3 6 9 
+//outside the loop9
+//this is incorrect. You forgot about the addition. variable declared. condition checked. body of code. check, augment, body.
+
