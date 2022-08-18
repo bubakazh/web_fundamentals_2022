43e8f3e23710f5b1ac9dceebf90aedc84a69d09a
bubakazh <troymanansala@gmail.com>
2022-08-18 12:07:54 -0700

    just everything

---
+++ algo/day_4.js
@@ -0,0 +1,12 @@
+// BIG 13 PRACTICE
+var sum = 0;
+
+function printintsum0255(){
+    for(let i = 0; i < 256; i++){
+        console.log(i);
+        sum += i;
+        console.log(sum);
+    }
+}
+
+printintsum0255();
