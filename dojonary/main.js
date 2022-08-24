71e9efd757d5b39e3c0c13780b10d20b66866e1d
bubakazh <troymanansala@gmail.com>
2022-08-24 14:09:01 -0700

    dojonary start to finish

---
+++ dojonary/main.js
@@ -0,0 +1,11 @@
+function ninjaLike(){
+    confirm("Ninja was liked!")
+}
+function deletes(element){
+    element.remove()
+}
+function inOut(element){
+    if(element.innerText = "Login"){
+        element.innerText = "Logout"
+    }
+}
